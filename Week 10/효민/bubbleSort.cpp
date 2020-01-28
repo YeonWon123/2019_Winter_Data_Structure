62a75826126584cc234d44edc11b8db375f94203
shimhm <sgn12345@naver.com>
2020-01-28 13:56:45 +0900

    week10 bubbleSort

---
+++ Week 10/효민/bubbleSort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void BubbleSort(int arr[], int n) {
+	int i, j;
+	int temp;
+
+	for (i = 0; i < n - 1; i++) {
+		for (j = 0; j < (n - i) - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+
+int main() {
+	int arr[4] = { 3,2,4,1 };
+	int i;
+
+	BubbleSort(arr, sizeof(arr) / sizeof(int));
+
+	for (int i = 0; i < 4; i++) {
+		cout << arr[i];
+	}
+	cout << '\n';
+	return 0;
+}
